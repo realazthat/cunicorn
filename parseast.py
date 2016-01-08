ee62f8cd16aa3187872a4ccf7708bc73094dd9d5
realazthat <realazthat@gmail.com>
2016-01-08 15:06:10 -0500

    Parsing the ast lines into a full AST

--- parseast.py
+++ parseast.py
@@ -6,12 +6,72 @@ import sys
 
 
 class AstLine:
-    def __init__(self, parent, name, line_data, children=[]):
+    def __init__(self, parent, name, identifer, line_data, children=[]):
         self.parent = parent
         self.name = name
+        self.identifer = identifer
         self.line_data = line_data
         self.children = list(children)
 
+class FunctionDeclLine(AstLine):
+    def __init__(self, parent, identifer, line_data, children=[]):
+        AstLine.__init__(self, parent, 'FunctionDecl', line_data)
+
+def parse_ast_line(parent, name, identifer, line_data):
+    if name == 'FunctionDecl':
+        return FunctionDeclLine(parent, name, identifer, line_data)
+
+    return AstLine(parent, name, identifer, line_data)
+
+class Ast:
+    
+    def __init__(self, parent, identifier, astline):
+        self.parent = parent
+        self.identifier = identifier
+        self.astline = astline
+        
+class FunctionAst(Ast):
+    def __init__(self, parent, astline, funcname, arguments, body):
+        Ast.__init__(self,parent,astline.identifier,astline)
+        self.funcname = funcname
+        self.arguments = list(arguments)
+        self.body = body
+class ParmVarDeclAst(Ast):
+    def __init__(self, parent, astline, varname, typename):
+        Ast.__init__(self,parent,astline.identifier,astline)
+        self.varname = varname
+        self.typename = typename
+        
+def to_ast(astline):
+    
+    if astline.name == 'FunctionDecl':
+        func = FunctionAst(None, astline, astline.identifier, funcname, arguments=[], body=[])
+        for childastline in astline.children:
+            if childastline.name == 'ParmVarDecl':
+                childast = to_ast(childastline)
+                childast.parent = func
+                func.arguments += [childast]
+            elif childastline.name == 'CompoundStmt':
+                body = to_ast(childastline)
+                body.parent = func
+                func.body = body
+            else:
+                assert False, childastline.name
+                
+        return func
+    elif astline.name == 'ParmVarDecl':
+        name,_,line_data_after_name = line_data.partition(' ')
+        identifer,_,line_data_after_id = line_data_after_name.partition(' ')
+        _,_,line_data_after_span = line_data_after_id.partition('> ')
+        _,_,line_data_col = line_data_after_span.partition(' ')
+        used,_,line_data_after_used = line_data_col.partition(' ')
+        varname,_,line_data_after_varname = line_data_after_used.partition(' ')
+
+        #todo: fix this
+        typename = ''
+        return ParmVarDeclAst(None, astline, varname, typename)
+    else:
+        assert False, astline.name
 
 def main():
     linelist = []
@@ -34,9 +94,9 @@ def main():
         else:
             assert current_parent is not None
     
-        name,_,line_data = line_data.partition(' ')
-        address,_,line_data = line_data.partition(' ')
-        astline = AstLine(current_parent, name, line_data)
+        name,_,line_data_after_name = line_data.partition(' ')
+        identifer,_,line_data_after_id = line_data_after_name.partition(' ')
+        astline = parse_ast_line(current_parent,name,identifer,line_data)
         
         current = astline
         linelist += [astline]
